3ca2d9116a1a0f57518d0164f21f90b894bb7c16
qbic2002 <golovinmax28@gmail.com>
2021-09-28 15:10:53 +0300

    chore: removed suddenly pushed lab-1 files 0_o :)

--- Shops/Program.cs
+++ Shops/Program.cs
@@ -1,28 +1,9 @@
-﻿using System;
-using System.Linq;
-using Shops.Entities;
-using Shops.Services;
-
-namespace Shops
+﻿namespace Shops
 {
     internal class Program
     {
-        private static ShopManager _shopManager = new ShopManager();
-
         private static void Main()
         {
-            Shop shop = _shopManager.AddShop("Shop", "Address");
-            for (int i = 0; i < 5; i++)
-            {
-                _shopManager.RegisterProduct(i.ToString());
-            }
-
-            for (int i = 0; i < 5; i++)
-            {
-                shop.AddProducts(_shopManager.ListOfProducts[i], (i + 1) * 2, i + 1);
-            }
-
-            shop.Products.Values.ToList().ForEach(productInfo => Console.WriteLine(productInfo.Price));
         }
     }
 }

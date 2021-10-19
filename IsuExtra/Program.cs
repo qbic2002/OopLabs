ec88477f10ce9dd1c33158ca3b426958243782f3
qbic2002 <golovinmax28@gmail.com>
2021-10-19 08:28:31 +0300

    fix: clear Main()

--- IsuExtra/Program.cs
+++ IsuExtra/Program.cs
@@ -12,17 +12,6 @@ namespace IsuExtra
     {
         private static void Main()
         {
-            // JoinTrainingGroupManager jM = new JoinTrainingGroupManager();
-            // JoinTrainingGroup jG = jM.AddJTG(Faculty.TIT);
-            var list = new List<int>
-            {
-                1,
-                2,
-                4,
-            };
-            ReadOnlyCollection<int> rolist = new ReadOnlyCollection<int>(list);
-            rolist.ToList().Add(7);
-            list.ForEach(Console.WriteLine);
         }
     }
 }
